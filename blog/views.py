b65c30b33283d03085846f83a1b769ec7f2887b9
Manish <itzmk108@gmail.com>
2018-10-17 22:10:55 +0530

    blog: basic things is working for now

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+
+def blog(request):
+    articles = Article.objects
+    return render(request, 'blog.html', {'articles': articles})
--- second/urls.py
+++ second/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from jobs import views
+
 
 urlpatterns = [
     path('', include('jobs.urls')),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
