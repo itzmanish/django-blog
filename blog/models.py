652f6f71b9627a81c8550586e6e35cfba0db93d0
Manish <itzmk108@gmail.com>
2018-10-17 22:21:20 +0530

    blog: added title in admin panel instead of default one, custom date format using strftime, some improvisation

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Article(models.Model):
     content = models.TextField()
     image = models.ImageField(upload_to='images/')
     created_At = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.content[:100]
+
+    def pretty_date(self):
+        return self.created_At.strftime('%b %m %Y')
